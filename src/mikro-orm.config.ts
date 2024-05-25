43e941e7dfb7f81f490caab33d2aca99942d4941
gonnagetbetter <denissss906@gmail.com>
2024-05-25 22:49:35 +0300

    Added mikro-orm config

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,21 +1,26 @@
-import {defineConfig} from "@mikro-orm/postgresql";
+import { defineConfig } from '@mikro-orm/postgresql';
+import { Migrator } from '@mikro-orm/migrations';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 export default defineConfig({
-    entities: ['dist/**/*.entity.js'],
-    entitiesTs: ['src/**/*.entity.ts'],
-    dbName: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    migrations: {
-        tableName: 'orm_migrations',
-        path: 'src/migrations',
-        transactional: true,
-        disableForeignKeys: true,
-        allOrNothing: true,
-        dropTables: true,
-        safe: false,
-        emit: 'ts',
-    },
+  entities: ['dist/**/*.entity.js'],
+  entitiesTs: ['src/**/*.entity.ts'],
+  dbName: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  migrations: {
+    tableName: 'orm_migrations',
+    path: 'src/migrations',
+    transactional: true,
+    disableForeignKeys: true,
+    allOrNothing: true,
+    dropTables: true,
+    safe: false,
+    emit: 'ts',
+  },
+  extensions: [Migrator],
 });
