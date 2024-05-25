5b138f2f05f983a6f83a14d28428b8c09031a85d
gonnagetbetter <denissss906@gmail.com>
2024-05-25 22:47:00 +0300

    Added imports and orm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CacheModule } from './cache/cache.module';
+import { OrganizationsModule } from './organizations/organizations.module';
+import { CustomersModule } from './customers/customers.module';
+import { HotelsModule } from './hotels/hotels.module';
+import { RoomsModule } from './rooms/rooms.module';
+import { BookingModule } from './booking/booking.module';
+import { MikroOrmCoreModule } from '@mikro-orm/nestjs/mikro-orm-core.module';
+import defineConfig from './mikro-orm.config';
 
 @Module({
-  imports: [CacheModule],
+  imports: [
+    CacheModule,
+    OrganizationsModule,
+    CustomersModule,
+    HotelsModule,
+    RoomsModule,
+    BookingModule,
+    MikroOrmCoreModule.forRoot(defineConfig),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
