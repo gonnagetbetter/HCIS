9009f4830a3bfe36591b22a15d689e15f2d3bc04
gonnagetbetter <denissss906@gmail.com>
2024-05-26 00:09:22 +0300

    started creating room's service and controller

--- src/rooms/rooms.controller.ts
+++ src/rooms/rooms.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { RoomsService } from './rooms.service';
+import { CreateRoomDto } from './dto/create-room.dto';
 
+@ApiTags('rooms')
 @Controller('rooms')
-export class RoomsController {}
+export class RoomsController {
+  constructor(private readonly roomService: RoomsService) {}
+  @Post()
+  create(@Body() createRoomDto: CreateRoomDto) {}
+}
--- src/rooms/rooms.service.ts
+++ src/rooms/rooms.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { CacheService } from '../cache/cache.service';
+import { RoomRepository } from './repositories/room.repository';
+import { EntityManager } from '@mikro-orm/core';
+import { HotelsService } from '../hotels/hotels.service';
+import { BookingService } from '../booking/booking.service';
+import { BasicCrudService } from '../common/basic-crud.service';
+import { Room } from './entities/room.entity';
+import { CreateRoomDto } from './dto/create-room.dto';
 
 @Injectable()
-export class RoomsService {}
+export class RoomsService extends BasicCrudService<Room> {
+  constructor(
+    protected readonly roomRepository: RoomRepository,
+    protected readonly cacheService: CacheService,
+    protected readonly entityManager: EntityManager,
+    protected readonly hotelsService: HotelsService,
+    protected readonly bookingService: BookingService,
+  ) {
+    super(Room, roomRepository, cacheService, entityManager);
+  }
+  async createRoom(dto: CreateRoomDto): Promise<Room> {
+    const hotel = await this.hotelsService.findOne(dto.hotelId);
+    const existing = await this.roomRepository.findOne({
+      roomNumber: dto.roomNumber,
+      hotel: hotel,
+    });
+    if (existing) {
+      throw new Error('Room already exists');
+    }
+    return this.createOne(dto);
+  }
+}
